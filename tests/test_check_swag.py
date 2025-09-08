8ddb5f6ae9f57e60ad7abff4c28fba3bfbf66d5e
AngryAlexQA <Alexa.bujor.qa@gmail.com>
2025-09-08 13:47:09 +0300

    Update test_check_swag.py

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -3,32 +3,31 @@ import pytest
 from pages.swag_labs import SwagLabs
 
 
-class TestSwagLabs:
-    """Тесты для страницы Swag Labs."""
+def test_check_icon(driver):
+    """Тест проверки наличия иконки."""
+    # Переходим на страницу
+    swag_page = SwagLabs(driver)
+    swag_page.visit()
 
-    def test_check_icon(self, driver):
-        """Тест проверки наличия иконки."""
-        # Переходим на страницу
-        swag_page = SwagLabs(driver)
-        swag_page.visit()
+    # Проверяем наличие иконки
+    assert swag_page.exist_icon() == True, "Иконка не найдена на странице"
 
-        # Проверяем наличие иконки
-        assert swag_page.exist_icon() == True, "Иконка не найдена на странице"
 
-    def test_check_username_field(self, driver):
-        """Тест проверки наличия поля имени пользователя."""
-        # Переходим на страницу
-        swag_page = SwagLabs(driver)
-        swag_page.visit()
+def test_check_username_field(driver):
+    """Тест проверки наличия поля имени пользователя."""
+    # Переходим на страницу
+    swag_page = SwagLabs(driver)
+    swag_page.visit()
 
-        # Проверяем наличие поля имени
-        assert swag_page.exist_username_field() == True, "Поле имени пользователя не найдено"
+    # Проверяем наличие поля имени
+    assert swag_page.exist_username_field() == True, "Поле имени пользователя не найдено"
 
-    def test_check_password_field(self, driver):
-        """Тест проверки наличия поля пароля."""
-        # Переходим на страницу
-        swag_page = SwagLabs(driver)
-        swag_page.visit()
 
-        # Проверяем наличие поля пароля
-        assert swag_page.exist_password_field() == True, "Поле пароля не найдено"
+def test_check_password_field(driver):
+    """Тест проверки наличия поля пароля."""
+    # Переходим на страницу
+    swag_page = SwagLabs(driver)
+    swag_page.visit()
+
+    # Проверяем наличие поля пароля
+    assert swag_page.exist_password_field() == True, "Поле пароля не найдено"
